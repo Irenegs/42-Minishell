ec11431c20fdb1ed3006217dd0ca23c80c258aca
Irenegs <gonzalezsaavedrairene@gmail.com>
2024-11-24 20:16:36 +0100

    redirections builtins

--- built_in_check.c
+++ built_in_check.c
@@ -32,7 +32,7 @@ int	is_builtin(char *cmd)
 		return (0);
 }
 
-int	execute_builtin(t_mix *data, char **command)
+static int	choose_and_execute_builtin(t_mix *data, char **command)
 {
 	if (ft_strcmp(command[0], "echo") == 0)
 		return (ft_echo(command));
@@ -49,4 +49,56 @@ int	execute_builtin(t_mix *data, char **command)
 	else if (ft_strcmp(command[0], "env") == 0)
 		return (ft_env(data));
 	return (1);
+}
+
+static int	redirect_input(t_mix *data, int *input, int *stdin_copy)
+{
+	*input = extract_input(data->input, data, -1);
+	if (*input > 0)
+	{
+		*stdin_copy = dup(0);
+		dup2(*input, STDIN_FILENO);
+	}
+	if (*input == -2)
+		return (1);
+	return (0);
+}
+
+static int	redirect_output(t_mix *data, int *output, int *stdout_copy)
+{
+	*output = extract_output(data->input, data);
+	if (*output > 0)
+	{
+		*stdout_copy = dup(1);
+		dup2(*output, STDOUT_FILENO);
+	}
+	if (*output == -2)
+		return (1);
+	return (0);
+}
+
+int	execute_builtin(t_mix *data, char **command)
+{
+	int input;
+	int output;
+	int return_value;
+	int stdin_copy;
+	int	stdout_copy;
+
+	if (redirect_input(data, &input, &stdin_copy) != 0)
+		return (1);
+	if (redirect_output(data, &output, &stdout_copy) != 0)
+		return (1);
+	return_value = choose_and_execute_builtin(data, command);
+	if (input > 0)
+	{
+		close(input);
+		dup2(stdin_copy, 0);
+	}
+	if (output > 0)
+	{
+		close(output);
+		dup2(stdout_copy, 1);
+	}
+	return (return_value);
 }
